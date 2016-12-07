b81990a01d127975c2e609f26d34cacd7e9ae8c9
yuanxinlei <649066464@qq.com>
2016-12-07 13:32:20 +0800

    fff

--- src/cn/edu/njtech/mvc/filter/LoginFilter.java
+++ src/cn/edu/njtech/mvc/filter/LoginFilter.java
@@ -25,20 +25,26 @@ public class LoginFilter implements Filter {
 		String path = request.getRequestURI();
 		//System.out.println(path);
 		User u=(User) request.getSession().getAttribute("User");
-		if(path.contains("login.jsp")||path.contains("wdetail")||path.contains("login")){
-			chain.doFilter(request, response);
-			return;
-		}
-		if(path.contains("login.jsp")){
+		if(path.contains("login.jsp")||path.contains("wdetail")||path.contains("login")||path.contains("fore.jsp")){
 			chain.doFilter(request, response);
 			return;
 		}else{
-			if(u==null){
+			if(u==null||u.getUsertype()!=1){
 				response.sendRedirect("login.jsp");
 			}else{
 				chain.doFilter(request, response);
 			}
 		}
+//		if(path.contains("login.jsp")){
+//			chain.doFilter(request, response);
+//			return;
+//		}else{
+//			if(u==null){
+//				response.sendRedirect("login.jsp");
+//			}else{
+//				chain.doFilter(request, response);
+//			}
+//		}
 	}
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {
