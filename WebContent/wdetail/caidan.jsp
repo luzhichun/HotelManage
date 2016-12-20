bc4ccb597fef6ed490433d6e1c300d90eef63e2b
MrNi <123>
2016-12-20 14:24:21 +0800

    更新

---
+++ WebContent/wdetail/caidan.jsp
@@ -0,0 +1,191 @@
+﻿<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<!-- 包含公共的JSP代码片段 -->
+
+<title>无线点餐平台</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="text/javascript" src="style/js/jquery-2.2.3.min.js"></script>
+<script type="text/javascript" src="style/js/page_common.js"></script>
+<script type="text/javascript" src="style/js/fly/jquery.fly.min.js"></script>
+<link href="style/css/common_style_blue.css" rel="stylesheet"
+	type="text/css">
+<link rel="stylesheet" type="text/css" href="style/css/index_1.css" />
+<link href="style/css/index.css" rel="stylesheet" type="text/css" />
+
+<script type="text/javascript">
+
+</script>
+</head>
+<body style="text-align: center">
+	<div id="all">
+		<div id="menu">
+			<!-- 显示菜品的div -->
+			<div id="top">
+				<ul>
+					<!-- 循环列出餐品 -->
+					<c:if test="${foodList!=null}">
+						<c:forEach items="${foodList}" var="fd">
+							<li>
+								<dl>
+									<dt>
+										<a href="foodInfor?foodId=${fd.foodId}"> <img
+											width="214px" height="145px" src="../upload/${fd.img}" />
+										</a>
+									</dt>
+									<dd class="f1">
+										<a href="foodInfor?foodId=${fd.foodId}">${fd.foodName}</a>
+									</dd>
+									<dd class="f2">
+										<a href="foodInfor?foodId=${fd.foodId}">&yen;${fd.price}</a>
+									</dd>
+								</dl>
+							</li>
+						</c:forEach>
+					</c:if>
+				</ul>
+			</div>
+
+			<!-- 底部分页导航条div -->
+			<c:if test="${type==0}">
+				<div id="foot">
+				<span
+					style="float: left; line-height: 53PX; margin-left: -50px; font-weight: bold;">
+					<span style="font-weight: bold"> <c:choose>
+							<c:when test="${FFcurrentPage>1}">
+								<a href="menus?FFcurrentPage=${FFcurrentPage-1}">&lt;&lt;</a>
+							</c:when>
+							<c:otherwise>
+								<a href="#">&lt;&lt;</a>
+							</c:otherwise>
+						</c:choose>
+				</span>
+				</span>
+				<div id="btn">
+					<ul>
+						<!-- 参看 百度, 谷歌是 左 5 右 4 -->
+						<c:forEach begin="1" end="${FFtotalPage}" var="p">
+							<c:choose>
+								<c:when test="${p==FFcurrentPage}">
+									<li><a href="menus?FFcurrentPage=${p}"style="color: #067db5">${p}</a></li>
+								</c:when>
+								<c:otherwise>
+									<li><a href="menus?FFcurrentPage=${p}">${p}</a></li>
+								</c:otherwise>
+							</c:choose>>
+					</c:forEach>
+					</ul>
+				</div>
+				<span
+					style="float: right; line-height: 53px; margin-right: 10px; font-weight: bold;">
+					<c:choose>
+						<c:when test="${FFcurrentPage<FFtotalPage}">
+							<a href="menus?FFcurrentPage=${FFcurrentPage+1}">&gt;&gt;</a>
+						</c:when>
+						<c:otherwise>
+							<a href="#">&gt;&gt;</a>
+						</c:otherwise>
+					</c:choose>
+
+				</span>
+			</div>		
+			</c:if>
+			<c:if test="${type==1}">
+				<div id="foot">
+				<span
+					style="float: left; line-height: 53PX; margin-left: -50px; font-weight: bold;">
+					<span style="font-weight: bold"> <c:choose>
+							<c:when test="${FFcurrentPage>1}">
+								<a href="menusByType?FFcurrentPage=${FFcurrentPage-1}">&lt;&lt;</a>
+							</c:when>
+							<c:otherwise>
+								<a href="#">&lt;&lt;</a>
+							</c:otherwise>
+						</c:choose>
+				</span>
+				</span>
+				<div id="btn">
+					<ul>
+						<!-- 参看 百度, 谷歌是 左 5 右 4 -->
+						<c:forEach begin="1" end="${FFtotalPage}" var="p">
+							<c:choose>
+								<c:when test="${p==FFcurrentPage}">
+									<li><a href="menusByType?FFcurrentPage=${p}"style="color: #067db5">${p}</a></li>
+								</c:when>
+								<c:otherwise>
+									<li><a href="menusByType?FFcurrentPage=${p}">${p}</a></li>
+								</c:otherwise>
+							</c:choose>>
+					</c:forEach>
+					</ul>
+				</div>
+				<span
+					style="float: right; line-height: 53px; margin-right: 10px; font-weight: bold;">
+					<c:choose>
+						<c:when test="${FFcurrentPage<FFtotalPage}">
+							<a href="menusByType?FFcurrentPage=${FFcurrentPage+1}">&gt;&gt;</a>
+						</c:when>
+						<c:otherwise>
+							<a href="#">&gt;&gt;</a>
+						</c:otherwise>
+					</c:choose>
+
+				</span>
+			</div>	
+			</c:if>
+		</div>
+
+		<!-- 右边菜系列表，菜品搜索框  -->
+		<div id="dish_class">
+			<div id="dish_top">
+				<ul>
+					<li class="dish_num"></li>
+					<li><a href="cart"> <img src="style/images/call2.gif" />
+					</a></li>
+				</ul>
+			</div>
+
+			<div id="dish_2">
+			<input id="btn_top" type="button" value="上一页" style="background:none">
+			<input id="currentPage" name="当前页" type="hidden" value="${FTcurrentPage}"/>
+			<input id="totalPage"name="总页数" type="hidden" value="${FTtotalPage}"/>
+				<ul id="typeList">
+					<c:if test="${ftl!=null }">
+						<c:forEach items="${ftl}" var="ft">
+							<li><a
+								href="menusByType?foodtypeid=${ft.foodtypeid}&&foodName=${null}&&FFcurrentPage=1&&type=1">${ft.typename}</a></li>
+						</c:forEach>
+					</c:if>
+				</ul>
+				<input id="btn_foot"  type="button" value="下一页" style="background:none">
+			</div>
+			<div id="dish_3">
+				<!-- 搜索菜品表单  -->
+				
+				<table width="166px">
+					<form action="menus" method="post">
+						<tr>
+							<td><input type="text" id="dish_name" name="key"
+								class="select_value" value="${foodName}"/> 
+								<input type="hidden" value="selectFood"
+								name="method"></td>
+						</tr>
+						<tr>
+							<td><input type="submit" id="sub" value="" name="f"/></td>
+						</tr>
+					
+					</form>
+						<tr>
+							<td><a href="menus?key=${null}&&type=0"> <img
+									src="style/images/look.gif" />
+							</a></td>
+						</tr>
+					</table>
+			</div>
+		</div>
+
+	</div>
+</body>
+</html>
---
+++ src/cn/edu/njtech/mvc/action/ForeMenuAction.java
@@ -0,0 +1,320 @@
+package cn.edu.njtech.mvc.action;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.sf.json.JSONObject;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import cn.edu.njtech.common.Page;
+import cn.edu.njtech.entity.Dinnertable;
+import cn.edu.njtech.entity.Food;
+import cn.edu.njtech.entity.Foodtype;
+import cn.edu.njtech.entity.OrderDetail;
+import cn.edu.njtech.entity.Orders;
+import cn.edu.njtech.service.IFoodService;
+import cn.edu.njtech.service.IForeService;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+public class ForeMenuAction extends ActionSupport implements ModelDriven<OrderDetail>, SessionAware{
+	private IForeService foreService;
+	private IFoodService foodService;
+	private int type=0;
+	
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public IFoodService getFoodService() {
+		return foodService;
+	}
+
+	public void setFoodService(IFoodService foodService) {
+		this.foodService = foodService;
+	}
+	private Map<String,Object> foreSession;
+	private int tableId;
+	private int OcurrentPage=1;
+	private String foodName;
+	private String key;
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+	private int foodtypeid;
+	private int foodId;
+	private int oid;
+	private OrderDetail od=new OrderDetail();
+	private double totalPay=0;
+	private int FFcurrentPage=1;
+	private int FTcurrentPage=1;
+	
+	public int getFTcurrentPage() {
+		return FTcurrentPage;
+	}
+
+	public void setFTcurrentPage(int fTcurrentPage) {
+		FTcurrentPage = fTcurrentPage;
+	}
+
+	public int getFFcurrentPage() {
+		return FFcurrentPage;
+	}
+
+	public void setFFcurrentPage(int fFcurrentPage) {
+		FFcurrentPage = fFcurrentPage;
+	}
+
+	@Override
+	public OrderDetail getModel() {
+		// TODO Auto-generated method stub
+		return od;
+	}
+	public int getFoodId() {
+		return foodId;
+	}
+
+	public void setFoodId(int foodId) {
+		this.foodId = foodId;
+	}
+
+	public int getFoodtypeid() {
+		return foodtypeid;
+	}
+
+	public void setFoodtypeid(int foodtypeid) {
+		this.foodtypeid = foodtypeid;
+	}
+
+	public String getFoodName() {
+		return foodName;
+	}
+
+	public void setFoodName(String foodName) {
+		this.foodName = foodName;
+	}
+
+	public int getOcurrentPage() {
+		return OcurrentPage;
+	}
+
+	public void setOcurrentPage(int ocurrentPage) {
+		OcurrentPage = ocurrentPage;
+	}
+
+	public int getTableId() {
+		return tableId;
+	}
+
+	public void setTableId(int tableId) {
+		this.tableId = tableId;
+	}
+	public String result;
+	
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+	public IForeService getForeService() {
+		return foreService;
+	}
+
+	public void setForeService(IForeService foreService) {
+		this.foreService = foreService;
+	}
+	@Override
+	public void setSession(Map<String, Object> arg0) {
+		// TODO Auto-generated method stub
+		this.foreSession=arg0;
+	}
+	public String getFreeTable(){
+		Map<String, Object> chatroom = new HashMap<String, Object>();
+		Page page=new Page(OcurrentPage,4);
+		foreSession.put("OcurrentPage", OcurrentPage);
+		List<Dinnertable> list = foreService.getFreeTable(page.getCurrentPage());
+		int totalNum=foreService.getFreeDinnerNum();
+		page.setTotalPage((totalNum-1)/page.getPageSize()+1);
+		foreSession.put("OtotalPage", page.getTotalPage());
+		chatroom.put("list", list);
+		JSONObject jo = JSONObject.fromObject(chatroom);
+		this.result = jo.toString();
+		return SUCCESS;
+	}
+	public String getFoodType(){
+		Map<String, Object> chatroom = new HashMap<String, Object>();
+		Page typePage=new Page(1, 4);
+		typePage.setCurrentPage(FTcurrentPage);
+		List<Foodtype> foodTypeList=foreService.getFoodType(typePage.getCurrentPage());
+		int total=foodService.selectAllFoodType("").size();
+		foreSession.put("FTtotalPage",(total-1)/typePage.getPageSize()+1);
+//		System.out.println();
+		foreSession.put("FTcurrentPage", FTcurrentPage);
+		chatroom.put("list", foodTypeList);
+		JSONObject jo = JSONObject.fromObject(chatroom);
+		this.result = jo.toString();
+		return SUCCESS;
+	}
+	public String menusList(){
+		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss ");
+		String date=sdf.format(new Date());
+		//判断餐桌的状态 要是餐桌的状态空闲就更新
+		Dinnertable checkDt=foreService.selectByTableId(tableId);
+		if(checkDt==null){
+			//更新餐桌
+			Dinnertable dt=new Dinnertable();
+			dt.setTableId(tableId);
+			dt.setOrderDate(date);
+			dt.setTableStatus(1);
+			foreService.updateTable(dt);
+		}
+		//判断订单的状态 如果订单已存在并且未结账就不用创建
+		Orders checkO=foreService.selectordersByTableId(tableId);
+		if(checkO==null){
+			//创建order
+			Orders o=new Orders();
+			o.setOrderDate(date);
+			o.setTableId(tableId);
+			o.setOrderStatus(0);
+			o.setTotalPrice(0.0);
+			foreService.addOrders(o);
+		}
+		Page typePage=new Page(1, 4);
+		List<Foodtype> foodTypeList=foreService.getFoodType(typePage.getCurrentPage());
+		int total=foodService.selectAllFoodType("").size();
+		foreSession.put("FTtotalPage",(total-1)/typePage.getPageSize()+1);
+		foreSession.put("FTcurrentPage", FTcurrentPage);
+		foreSession.put("ftl", foodTypeList);
+		Map<String,Object> map=new HashMap<String, Object>();
+		String keyword=key;
+		if(keyword==null){
+			keyword="";
+		}
+		Page page=new Page(1, 6);
+		page.setCurrentPage(FFcurrentPage);
+		map.put("foodName", keyword);
+		map.put("currentPage",page.getCurrentPage());
+		map.put("pageSize",page.getPageSize());
+		List<Food> foodList=foreService.showFoodMenu(map);
+		int ffsize=foreService.getFood();
+		page.setTotalPage((ffsize-1)/page.getPageSize()+1);
+		foreSession.put("FFtotalPage", page.getTotalPage());
+		foreSession.put("foodList", foodList);
+		foreSession.put("FFcurrentPage",FFcurrentPage);
+		foreSession.put("type",type);
+		foreSession.put("foodName",keyword);
+		return SUCCESS;
+	}
+	public String menusByType(){
+		Map<String,Object>map=new HashMap<String, Object>();
+		if(foodName==null){
+			foodName="";
+		}
+		Page page=new Page(1, 6);
+		page.setCurrentPage(FFcurrentPage);
+		map.put("foodName", foodName);
+		map.put("currentPage",page.getCurrentPage());
+		map.put("pageSize",page.getPageSize());
+		map.put("foodType_id", foodtypeid);
+		int ffsize=foreService.getFoodByFoodType(foodtypeid);
+		page.setTotalPage((ffsize-1)/page.getPageSize()+1);
+		List<Food> foodList=foreService.showFoodMenuByTypeId(map);
+		foreSession.put("foodList", foodList);
+		foreSession.put("FFtotalPage", page.getTotalPage());
+		foreSession.put("FFcurrentPage",FFcurrentPage);
+		foreSession.put("type",type);
+		return SUCCESS;
+	}
+	public String foodInformation(){
+		Food food=foreService.selectByFoodId(foodId);
+		foreSession.put("sFood", food);
+		return SUCCESS;
+	}
+	public String createOrderDetail(){
+		Orders checkO=foreService.selectordersByTableId(tableId);
+		oid=checkO.getOrdersId();
+		OrderDetail od=new OrderDetail();
+		od.setFood_id(foodId);
+		od.setOrderId(checkO.getOrdersId());
+		od.setFoodCount(1);
+		List<OrderDetail> list=foreService.getOrderDetailByOrderId(od);
+		if(list.size()==0){
+			//如果没有就创建
+			foreService.addOrderDetail(od);
+		}else{
+			OrderDetail ood=list.get(0);
+			if(foodId==ood.getFood_id()){
+				//如果菜品是一样的就改变菜的数量如果不一样就创建详细
+				ood.setFoodCount(ood.getFoodCount()+1);
+				foreService.updateDetail(ood);
+			}else{
+				foreService.addOrderDetail(od);
+			}
+		}
+		return SUCCESS;
+	}
+	public String dealOrderDetail(){
+		List<OrderDetail> odlist=foreService.selectOrderDetailById(oid);
+		totalPay=0;
+		for (OrderDetail orderDetail : odlist) {
+			totalPay=orderDetail.getPrice()*orderDetail.getFoodCount()+totalPay;
+		}
+		foreSession.put("odList", odlist);
+		foreSession.put("totalPay", totalPay);
+		return SUCCESS;
+	}
+	public String changeCount(){
+//		System.out.println(od);
+		foreService.updateDetail(od);
+		List<OrderDetail> odlist=foreService.selectOrderDetailById(od.getOrderId());
+		totalPay=0;
+		for (OrderDetail orderDetail : odlist) {
+			totalPay=orderDetail.getPrice()*orderDetail.getFoodCount()+totalPay;
+		}
+		Map<String, Object> chatroom = new HashMap<String, Object>();
+		chatroom.put("totalPay", totalPay);
+		JSONObject jo = JSONObject.fromObject(chatroom);
+		this.result = jo.toString();
+		return SUCCESS;
+	}
+	public String deleteDetail(){
+		foreService.deleteDetail(od);
+		return SUCCESS;
+	}
+	public String createOrder(){
+		Orders o=new Orders();
+		o.setOrdersId(oid);
+		o.setTotalPrice(totalPay);
+		o.setOrderStatus(0);
+		foreService.updateOrder(o);
+		List<OrderDetail> odlist=foreService.selectOrderDetailById(oid);
+		foreSession.put("odList", odlist);
+		foreSession.put("totalPay", totalPay);
+		return SUCCESS;
+	}
+	
+	public String jiezhang(){
+		Orders o=new Orders();
+		o.setOrdersId(oid);
+		o.setTotalPrice(totalPay);
+		o.setOrderStatus(1);
+		foreService.updateOrder(o);
+		return SUCCESS;
+	}
+}
